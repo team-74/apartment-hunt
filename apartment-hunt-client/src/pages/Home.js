22603f0fd63a56c9f15f28ec5c23f353c7fd83dc
Tazwar <tazwar9t6@gmail.com>
2020-11-15 22:26:56 +0600

    added navbar but still cant fix the bug of sass version

---
+++ apartment-hunt-client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainNav from '../Components/Header/MainNav/MainNav';
+
+const Home = () => {
+    return (
+        <div>
+            <MainNav/>
+        </div>
+    );
+};
+
+export default Home;
